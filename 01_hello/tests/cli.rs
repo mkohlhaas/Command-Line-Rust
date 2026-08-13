d969b641cbd005152e3f0915475bbdf501a4a518
Hans-J. Schmid <knock@myopendoor.de>
2026-08-13 16:28:13 +0200

    ch01

--- 01_hello/tests/cli.rs
+++ 01_hello/tests/cli.rs
@@ -2,12 +2,30 @@ use assert_cmd::Command;
 use pretty_assertions::assert_eq;
 
 #[test]
-fn runs() {
+fn runs_ls_1() {
+    let mut cmd = Command::new("ls");
+    cmd.assert().success();
+}
+
+#[test]
+fn runs_ls_2() {
+    let mut cmd = Command::cargo_bin("hello").unwrap();
+    cmd.assert().success();
+}
+
+#[test]
+fn runs_1() {
     let mut cmd = Command::cargo_bin("hello").unwrap();
     let output = cmd.output().expect("fail");
     assert!(output.status.success());
     let stdout = String::from_utf8(output.stdout).expect("invalid UTF-8");
     assert_eq!(stdout, "Hello, world!\n");
+}
+
+#[test]
+fn runs_2() {
+    let mut cmd = Command::cargo_bin("hello").unwrap();
+    cmd.assert().success().stdout("Hello, world!\n");
 }
 
 #[test]
